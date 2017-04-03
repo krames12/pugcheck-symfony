c5ec01ab4e73b9b3bbab818fd84286d33524e7a5
krames <krames@centos-pugcheck.(none)>
2017-04-03 20:07:57 +0000

    recieves blizz character info. grabs raid specified

--- src/AppBundle/Controller/CharacterController.php
+++ src/AppBundle/Controller/CharacterController.php
@@ -19,23 +19,53 @@ class CharacterController extends Controller
      */
 
     public function characterShow($region, $serverName, $characterName) {
+        $wclKey = getEnv('WCL_KEY');
+        $blizzKey = getEnv('BLIZZ_KEY');
+        $cleanName = $characterName; //use $this->cleanName in the future once configured
+        $blizzUrl = 'https://'.$region.'.api.battle.net/wow/character/'.$serverName.'/'.$cleanName.'?fields=progression,items&locale=en_US&apikey='.$blizzKey;
+        $wclUrl = 'https://www.warcraftlogs.com:443/v1/rankings/character/'.$cleanName.'/'.$serverName.'/'.$region.'?api_key='.$wclKey;
+
+        $currentRaid = 'The Nighthold';
+
+        $characterData = json_decode($this->getCharacterInfo($blizzUrl));
+        $progressionData = $this->parseProgressionData($characterData->progression->raids);
+
         return $this->render('default/character.html.twig', array(
             'region' => $region,
-            'serverName' => $serverName,
-            'characterName' => $characterName)
-        );
+            'serverName' => $characterData->realm,
+            'characterName' => $characterData->name,
+            'itemLevel' => $characterData->items->averageItemLevel,
+            'progressionData' => $progressionData,
+            'data' => $characterData
+        ));
     }
 
-    protected function getCharacterInfo($region, $serverName, $characterName) {
+    protected function getCharacterInfo($requestUrl) {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $requestUrl
+        ));
 
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
     }
 
     protected function cleanCharacterName($characterName) {
 
     }
 
-    protected function parseCharacterInfo($characterInfo) {
+    protected function parseProgressionData($data) {
+        $currentRaidData = array_filter($data, function($raid) {
+            if($raid->name == 'The Nighthold') {
+                return $raid;
+                return $raid;
+            }
 
+        });
+
+        return $currentRaidData[37];
     }
 
     protected function difficultyProgress($difficulty, $bossData) {
